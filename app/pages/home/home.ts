80ef1e46f4b4ff3a44b7bebef36257c0053723f8
EliasSimoes <eliasunipdesign@gmail.com>
2016-09-20 05:43:37 -0300

    Adicionado as views categoria e produto

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductPage } from './../product/product';
+import { CategoryPage } from './../category/category';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
 
+  tabRoot1: any;
+  tabRoot2: any;
+
+  constructor(public navCtrl: NavController) {
+    this.tabRoot1 = ProductPage;
+    this.tabRoot2 = CategoryPage;
   }
 }
